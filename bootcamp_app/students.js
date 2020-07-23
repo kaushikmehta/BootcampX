97977ca264fff01bb0f3699f3369447394aeb006
Kaushik Mehta <kaushikmehta30@gmail.com>
2020-07-23 00:17:47 +0000

    parametrizes queries

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -3,6 +3,16 @@ const { Pool } = require('pg');
 const cohort = process.argv[2];
 const limit = process.argv[3]
 
+const values = [`%${cohort}%`, limit];
+
+const queryString = `
+SELECT students.id, students.name AS student, cohorts.name as cohort
+FROM students
+JOIN cohorts ON cohorts.id = cohort_id
+WHERE cohorts.name LIKE $1
+LIMIT $2;
+`;
+
 const pool = new Pool({
   user: 'vagrant',
   password: '123',
@@ -11,14 +21,10 @@ const pool = new Pool({
 });
 
 
-pool.query(`
-SELECT students.id, students.name AS student, cohorts.name as cohort
-FROM students
-JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name LIKE '%${cohort}%'
-LIMIT ${limit};
-`)
+pool.query(queryString, values)
 .then(res => {
   console.log(res.rows);
 })
 .catch(err => console.error('query error', err.stack));
+
+pool.end()
--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -1,7 +1,19 @@
 const { Pool } = require('pg');
 
 const cohort = process.argv[2];
-const limit = process.argv[3]
+const values = [`%${cohort}%`];
+
+
+const queryString = `
+SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+FROM teachers
+JOIN assistance_requests ON teacher_id = teachers.id
+JOIN students ON student_id = students.id
+JOIN cohorts ON cohort_id = cohorts.id
+WHERE cohorts.name LIKE $1
+ORDER BY teacher;
+`;
+
 
 const pool = new Pool({
   user: 'vagrant',
@@ -11,15 +23,7 @@ const pool = new Pool({
 });
 
 
-pool.query(`
-SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
-FROM teachers
-JOIN assistance_requests ON teacher_id = teachers.id
-JOIN students ON student_id = students.id
-JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name LIKE '%${cohort}%'
-ORDER BY teacher;
-`)
+pool.query(queryString, values)
 .then(res => {
   console.log(res.rows);
 })
